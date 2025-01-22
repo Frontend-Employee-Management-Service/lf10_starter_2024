7c69552e37b83dae585f22db82dd17336e4aefc7
Pichanee Schmidt <pichanee.s1@schule.bremen.de>
2025-01-22 09:29:31 +0100

    [Text-Filter] component

---
+++ src/app/text-filter/text-filter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-filter',
+  imports: [],
+  templateUrl: './text-filter.component.html',
+  styleUrl: './text-filter.component.css'
+})
+export class TextFilterComponent {
+
+  filterResults(value: string) {
+    
+  }
+}
