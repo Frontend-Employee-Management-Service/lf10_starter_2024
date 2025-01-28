e0464f936ad917a028ff971da9703d0dfae53075
Pichanee Schmidt <pichanee.s1@schule.bremen.de>
2025-01-29 00:32:35 +0100

    [Text Filter Component] - fixes

--- src/app/text-filter/text-filter.component.ts
+++ src/app/text-filter/text-filter.component.ts
@@ -1,18 +1,23 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-text-filter',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './text-filter.component.html',
-  styleUrl: './text-filter.component.css'
+  styleUrl: './text-filter.component.css',
+  standalone:true
 })
 export class TextFilterComponent {
   @Input() title: string = '';
-  @Input() value: string = '';
+  value: string = '';
   @Input() action: string = '';
   @Output() eventTriggered = new EventEmitter<{ action: string; value: string; event: string }>();
 
   onInputEvent(eventType: string) {
+    console.log(`${eventType},${this.value}`);
     const result = {
       action: this.action,
       value: this.value,
