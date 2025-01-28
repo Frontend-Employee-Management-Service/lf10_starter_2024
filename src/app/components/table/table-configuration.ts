94f7b7cbd3f83cba58bbe1d0fe1387f4f24e66cb
lorenz.r1 <lorenz.r1@schule.bremen.de>
2025-01-28 19:03:18 +0100

    improved the comment for the labeling in tables-configuration. first time for everything :)

--- src/app/components/table/table-configuration.ts
+++ src/app/components/table/table-configuration.ts
@@ -6,7 +6,7 @@ import { DataCache } from "../../services/data-cache";
 export class TableConfiguration<T extends {id? :number}> {
   constructor(
     private cachingService: DataCache<T>,
-    public labels: Label<T>[],        // header order and naming
+    public labels: Label<T>[],        // defines order of column headers and maps the model properties to them
     public deleteActivated: boolean,
     public selection: SelectionBehaviour, 
     public routing: Routing           
