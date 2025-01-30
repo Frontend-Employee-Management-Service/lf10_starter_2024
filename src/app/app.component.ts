16e08ad27fcb3537641ff95db4e72d6001907b07
lorenz.r1 <lorenz.r1@schule.bremen.de>
2025-01-30 11:27:47 +0100

    routing struggle no1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
-import { NgModule , Component} from '@angular/core';
+import {Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
-import {EmployeeComponent} from "./employee/employee.component";
-import {EmployeeFormComponent} from "./components/employee-form/employee-form.component";
-import {RouterOutlet} from "@angular/router";
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule, EmployeeListComponent, NavbarComponent, EmployeeComponent, RouterOutlet],
+  imports: [FormsModule, CommonModule, NavbarComponent, RouterModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
-
 
 
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import {Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
 import {AppAuthGuard} from "./app.authguard";
-import {EmployeeComponent} from "./employee/employee.component";
-import {QualificationComponent} from "./qualification/qualification.component";
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { QualificationListComponent } from './qualification-list/qualification-list.component';
+import { Employee } from './models/Employee';
+import { EmployeeComponent } from './employee/employee.component';
 
-export const routes: Routes = [ {path: '', component: AppComponent, canActivate: [AppAuthGuard]}
-, {path: 'employee', component: EmployeeComponent, canActivate: [AppAuthGuard]},
-  {path: 'qualification', component: QualificationComponent, canActivate: [AppAuthGuard]}];
+export const routes: Routes = [ 
+  {path: '', component: EmployeeComponent, canActivate: [AppAuthGuard]},
+  {path: 'employees', title: "employees", component: EmployeeListComponent, canActivate: [AppAuthGuard]},
+  {path: 'qualifications', title: "employees", component: QualificationListComponent, canActivate: [AppAuthGuard]},
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+];
