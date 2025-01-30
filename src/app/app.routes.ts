0b37e9ad9f02a8fe6714202318d28ae1be700056
lorenz.r1 <lorenz.r1@schule.bremen.de>
2025-01-30 12:30:19 +0100

    fixed navbar routing, reworked authgard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import {Routes} from '@angular/router';
 import {AppAuthGuard} from "./app.authguard";
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { QualificationListComponent } from './qualification-list/qualification-list.component';
-import { Employee } from './models/Employee';
-import { EmployeeComponent } from './employee/employee.component';
+
 
 export const routes: Routes = [ 
-  {path: '', component: EmployeeComponent, canActivate: [AppAuthGuard]},
-  {path: 'employees', title: "employees", component: EmployeeListComponent, canActivate: [AppAuthGuard]},
+  {path: 'employees', title: "employees", component: EmployeeListComponent, canActivate: [AppAuthGuard]}, 
   {path: 'qualifications', title: "employees", component: QualificationListComponent, canActivate: [AppAuthGuard]},
   { path: '', redirectTo: '/employees', pathMatch: 'full' },
 ];
