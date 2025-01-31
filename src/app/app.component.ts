9ddfc9dd31e44aebc27d29674d836f1816a255fe
zerokrezan <zerokrezan@hotmail.de>
2025-01-31 13:24:16 +0100

    update employee component page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgModule , Component} from '@angular/core';
+import {NgModule, Component, Signal, signal} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
@@ -6,16 +6,23 @@ import {NavbarComponent} from "./components/navbar/navbar.component";
 import {EmployeeComponent} from "./employee/employee.component";
 import {EmployeeFormComponent} from "./components/employee-form/employee-form.component";
 import {RouterOutlet} from "@angular/router";
+import {TableComponent} from "./components/table/table.component";
+import {Label} from "./components/table/label";
+import {TableConfiguration} from "./components/table/table-configuration";
+import {DataCache} from "./services/data-cache";
+import {SelectionBehaviour} from "./components/table/selection-behaviour";
+import {Routing} from "./components/table/routing";
+import {EmployeesCacheService} from "./services/employees-cache.service";
+import {EmployeeService} from "./services/employee.service";
+import {Cache} from "@angular/cli/lib/config/workspace-schema";
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule, EmployeeListComponent, NavbarComponent, EmployeeComponent, RouterOutlet],
+  imports: [FormsModule, CommonModule, EmployeeListComponent, NavbarComponent, EmployeeComponent, RouterOutlet, TableComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
-
-
 
 export class AppComponent {
   title = 'lf10StarterNew';
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,23 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal, WritableSignal} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {EmployeeFormComponent} from "../components/employee-form/employee-form.component";
 import {CommonModule} from "@angular/common";
 import {Employee} from "../models/Employee";
 import {EmployeesCacheService} from "../services/employees-cache.service";
+import {ButtonComponent} from "../components/button/button.component";
+import {TableComponent} from "../components/table/table.component";
+import {TableConfiguration} from "../components/table/table-configuration";
+import {Label} from "../components/table/label";
+import {SelectionBehaviour} from "../components/table/selection-behaviour";
+import {QualificationsCacheService} from "../services/qualifications-cache.service";
+import {Qualification} from "../models/Qualification";
+import {Routing} from "../components/table/routing";
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [EmployeeFormComponent, FormsModule, CommonModule],
+  imports: [EmployeeFormComponent, FormsModule, CommonModule, ButtonComponent, TableComponent],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent implements OnInit{
   employee!: Employee;
+  configuration!: TableConfiguration<Employee>;
+  qualifications: WritableSignal<Qualification[]> = signal([new Qualification(1, 'Java'), new Qualification(2, 'Python')]);
 
-  constructor(private employeeCacheService:EmployeesCacheService) {
+
+
+  constructor(private employeeCacheService:EmployeesCacheService, private qualificationCacheService
+  :QualificationsCacheService) {
+
 
   }
+
+
 
   // Handle updates from the child
   updateEmployeeData(data: Employee) {
@@ -35,5 +51,17 @@ export class EmployeeComponent implements OnInit{
   ngOnInit(): void {
     this.employee = new Employee();
     this.employeeCacheService.refresh();
+    const labels : Label <Qualification> [] = [
+      new Label('id', 'ID'),
+      new Label('skill', 'Qualification'),
+    ]
+    const selectionBehaviour = new SelectionBehaviour(false,'');
+    const routing = new Routing(false, '');
+
+    this.configuration = new TableConfiguration<Qualification>(
+      this.qualificationCacheService, labels, true,selectionBehaviour, routing
+
+    )
+
   }
 }
