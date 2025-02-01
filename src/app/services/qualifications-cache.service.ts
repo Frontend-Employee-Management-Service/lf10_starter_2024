5f4919df13fa9e129217e625245bfef2655a05d6
lorenz.r1 <lorenz.r1@schule.bremen.de>
2025-02-01 22:15:58 +0100

    AdHocCache added as a 'dummy'-CachingService for the edit and new pages. also added better state change detection for caching services

--- src/app/services/qualifications-cache.service.ts
+++ src/app/services/qualifications-cache.service.ts
@@ -8,10 +8,10 @@ import { DataCache } from './data-cache';
   providedIn: 'root'
 })
 export class QualificationsCacheService extends DataCache<Qualification> {
-  
+
   private static cache: WritableSignal<Qualification[]> = signal<Qualification[]>([]);
   private static selected: Map<string, Qualification[]> = new Map<string, Qualification[]>();
-  
+
   constructor(private dataService: QualificationService) {
     super();
   }
@@ -29,6 +29,7 @@ export class QualificationsCacheService extends DataCache<Qualification> {
     const subscription: Subscription = this.dataService.selectAll()
       .subscribe(
         (qualifications: Qualification[]) => {
+          this.notifyStateChange();
           qualifications.forEach(entry => data.push(entry));
         }
       );
@@ -44,11 +45,13 @@ export class QualificationsCacheService extends DataCache<Qualification> {
   insert(qualification: Qualification): void {
     const result$: Observable<Qualification> = this.dataService.insert(qualification);
     const subscription: Subscription = result$.subscribe(
-      value => QualificationsCacheService.cache.update(data => {
-        data.push(value);
-        return data;
+      value => {
+        this.notifyStateChange();
+        QualificationsCacheService.cache.update(data => {
+          data.push(value);
+          return data;
+        })
       }
-      )
     );
     this.subscriptions.push(subscription);
   }
@@ -56,41 +59,45 @@ export class QualificationsCacheService extends DataCache<Qualification> {
   update(qualification: Qualification): void {
     const result$: Observable<Qualification> = this.dataService.update(qualification);
     const subscription: Subscription = result$.subscribe(
-      value => QualificationsCacheService.cache.update(data => {
-        data = data.filter(entry => entry.id !== qualification.id);
-        data.push(value);
-        return data;
+      value => {
+        this.notifyStateChange();
+        QualificationsCacheService.cache.update(data => {
+          data = data.filter(entry => entry.id !== qualification.id);
+          data.push(value);
+          return data;
+        }
+        )
       }
-      )
     );
     this.subscriptions.push(subscription);
   }
 
   delete(id: number): void {
-    this.isLoading.update(loadingIDs => loadingIDs.add(id)); 
+    this.isLoading.update(loadingIDs => loadingIDs.add(id));
     console.log(this.isLoading())
     const subscription: Subscription = this.dataService.delete(id)
-    .pipe(
-      take(1),
-      catchError(error=>{
-        this.isLoading.update(loadingIDs => {
-          loadingIDs.delete(id);
-          return loadingIDs;
-        }) 
-        throw new Error(error);
-      } 
-      ))
-    .subscribe(
-      {
-        complete: () =>  {
+      .pipe(
+        take(1),
+        catchError(error => {
           this.isLoading.update(loadingIDs => {
-            QualificationsCacheService.cache.update(values => values.filter(entry => entry.id !== id))
             loadingIDs.delete(id);
             return loadingIDs;
           })
+          throw new Error(error);
         }
-      }
-    );    
+        ))
+      .subscribe(
+        {
+          complete: () => {
+            this.notifyStateChange();
+            this.isLoading.update(loadingIDs => {
+              QualificationsCacheService.cache.update(values => values.filter(entry => entry.id !== id))
+              loadingIDs.delete(id);
+              return loadingIDs;
+            })
+          }
+        }
+      );
     this.subscriptions.push(subscription);
   }
 }
