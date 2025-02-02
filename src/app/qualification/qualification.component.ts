5788f1d15ae54f79f476d1da265478f89a1d7e1b
zerokrezan <zerokrezan@hotmail.de>
2025-02-02 19:35:38 +0100

    Update Qualification Component Edit/New Qualification -> fetch current qualification and employees who have this qualification

--- src/app/qualification/qualification.component.ts
+++ src/app/qualification/qualification.component.ts
@@ -8,13 +8,16 @@ import {QualificationFormComponent} from "../components/qualification-form/quali
 import {ActivatedRoute} from "@angular/router";
 import {QualificationService} from "../services/qualification.service";
 import {QualificationsCacheService} from "../services/qualifications-cache.service";
-import {Subscription} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {Qualification} from "../models/Qualification";
 import {Label} from "../components/table/label";
 import {SelectionBehaviour} from "../components/table/selection-behaviour";
 import {Routing} from "../components/table/routing";
 import {TableConfiguration} from "../components/table/table-configuration";
 import {Employee} from "../models/Employee";
+import {EmployeeFilterService} from "../services/employee-filter.service";
+import {EmployeesCacheService} from "../services/employees-cache.service";
+import {EmployeeService} from "../services/employee.service";
 
 @Component({
   selector: 'app-qualification',
@@ -27,12 +30,14 @@ export class QualificationComponent implements OnInit, OnDestroy{
 
   private activatedRoute = inject(ActivatedRoute);
   id!: number;
-  qualification!: Qualification;
+  qualification!: Qualification | undefined;
   private subscription!: Subscription;
   configuration!: TableConfiguration<Qualification>;
   employees!: Employee[];
 
-  constructor(private qualificationService: QualificationService,private qualificationCacheService: QualificationsCacheService) {
+  constructor(private qualificationService: QualificationService,private qualificationCacheService: QualificationsCacheService,
+              private employeeFilter:EmployeeFilterService, private employeeCacheService:EmployeesCacheService,
+              private employeeService: EmployeeService) {
   }
 
 
@@ -40,12 +45,19 @@ export class QualificationComponent implements OnInit, OnDestroy{
     this.id = this.activatedRoute.snapshot.params['id'];
     console.log("id:"+ this.id);
 
-    this.subscription = this.qualificationService.select(this.id)
-      .subscribe((qualification: Qualification) => {
-        this.qualification = qualification;
-      });
+    this.subscription = this.qualificationService.selectAll().
+      subscribe((qualifications: Qualification[]) => {
+        this.qualification = qualifications.find(qualification => qualification.id == this.id);
+        console.log("qualification:"+ this.qualification?.skill);
+      }
+    )
 
-    console.log("qualification:"+ this.qualification);
+    this.employeeCacheService.refresh();
+
+    this.employeeService.selectAll().subscribe((employees: Employee[]) => {
+      this.employees = this.employeeFilter.filterEmployeesByQualificationId(employees, this.id);
+      console.log("employees:", this.employees);
+    });
 
     const labels : Label <Employee> [] = [
       new Label('id', 'ID'),
@@ -55,8 +67,10 @@ export class QualificationComponent implements OnInit, OnDestroy{
     const selectionBehaviour = new SelectionBehaviour(false,'');
     const routing = new Routing(false, '');
 
-    this.configuration = new TableConfiguration<Qualification>(
-      this.qualificationCacheService, labels, true,selectionBehaviour, routing
+    console.log("employees:", this.employees);
+
+    this.configuration = new TableConfiguration<Employee>(
+      this.employeeCacheService, labels, true,selectionBehaviour, routing
     )
   }
 
@@ -68,3 +82,4 @@ export class QualificationComponent implements OnInit, OnDestroy{
 
   protected readonly signal = signal;
 }
+
