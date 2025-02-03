e43f1b2342debec99bbb1a373cb65a696bbb1020
lorenz.r1 <lorenz.r1@schule.bremen.de>
2025-02-03 23:20:28 +0100

    employee edit/new finished. couple of refactorings and a lot of ascynchronous struggling :(

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,81 +1,110 @@
-import {Component, inject, OnInit, Signal, computed, signal, WritableSignal, OnDestroy} from '@angular/core';
+import { Component, inject, OnInit, Signal, computed, DoCheck } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {EmployeeFormComponent} from "../components/employee-form/employee-form.component";
-import {CommonModule} from "@angular/common";
-import {Employee} from "../models/Employee";
-import {EmployeesCacheService} from "../services/employees-cache.service";
-import {ButtonComponent} from "../components/button/button.component";
-import {TableComponent} from "../components/table/table.component";
-import {TableConfiguration} from "../components/table/table-configuration";
-import {Label} from "../components/table/label";
-import {SelectionBehaviour} from "../components/table/selection-behaviour";
-import {QualificationsCacheService} from "../services/qualifications-cache.service";
-import {Qualification} from "../models/Qualification";
-import {Routing} from "../components/table/routing";
-import {ActivatedRoute} from "@angular/router";
-import {EmployeeService} from "../services/employee.service";
-import {Subscription} from "rxjs";
-import {QualificationService} from "../services/qualification.service";
+import { EmployeeFormComponent } from "../components/employee-form/employee-form.component";
+import { CommonModule } from "@angular/common";
+import { Employee } from "../models/Employee";
+import { EmployeesCacheService } from "../services/employees-cache.service";
+import { ButtonComponent } from "../components/button/button.component";
+import { TableComponent } from "../components/table/table.component";
+import { TableConfiguration } from "../components/table/table-configuration";
+import { Label } from "../components/table/label";
+import { SelectionBehaviour } from "../components/table/selection-behaviour";
+import { QualificationsCacheService } from "../services/qualifications-cache.service";
+import { Qualification } from "../models/Qualification";
+import { Routing } from "../components/table/routing";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [EmployeeFormComponent, FormsModule, CommonModule, ButtonComponent, TableComponent],
+  imports: [EmployeeFormComponent, FormsModule, CommonModule, ButtonComponent, TableComponent, RouterLink],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent implements OnInit, OnDestroy{
+export class EmployeeComponent implements OnInit, DoCheck {
   private activatedRoute = inject(ActivatedRoute);
   id!: number;
-  private subscription!: Subscription;
-
-  formDataEmployee: WritableSignal<Employee> = signal(new Employee());
-
-  emp!: Employee;
+  displayedQualificationsSignal!: Signal<Qualification[]>;
+  isComponentDataLoaded: boolean = false;
+  formDataEmployee: Employee | undefined= undefined;
+  selectedData!: Qualification[];
+  employeeSigal!: Signal<Employee>;
   configuration!: TableConfiguration<Employee>;
 
-  constructor(private qualificationCacheService
-  :QualificationsCacheService, public employeeService: EmployeeService) {
-  }
-
-
-  // Handle updates from the child
-  updateEmployeeData(data: Employee) {
-    this.formDataEmployee.set(data); // Update local state
-  }
-
-  // Handle final form submission
-  submitDataToBackend() {
-    if (this.formDataEmployee()){
-      this.employeeService.insert(this.emp);
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
+  constructor(
+    private qualificationCache: QualificationsCacheService,
+    private employeeCache : EmployeesCacheService,
+    private employeeService: EmployeeService
+  ) {}
+  
   ngOnInit(): void {
+    this.employeeCache.refresh();
     this.id = this.activatedRoute.snapshot.params['id'];
-
-    this.subscription = this.employeeService.select(this.id).
-      subscribe((employee: Employee) => {
-      this.emp = employee;
+    this.computeFormContent();
+    this.computeTableContent();
+    this.employeeCache.notifyStateChange();
+    this.configureTable();
+    this.selectedData = this.qualificationCache.withdrawSelected(this.activatedRoute.snapshot.url.join("/")); 
+  }
+  
+  private computeFormContent() {
+    this.employeeSigal = computed(() => {
+      this.employeeCache.detectStateChange();
+      const e = this.employeeCache.select(this.id);
+      this.formDataEmployee = e;
+      return e ?? new Employee();
     });
+  }
 
-    const labels : Label <Qualification> [] = [
+  private computeTableContent() {
+    this.displayedQualificationsSignal = computed(() => {
+      this.employeeCache.detectStateChange();
+      let qualifications: Qualification[] = [];
+      if (this.id) {
+        const e = this.employeeCache.select(this.id);
+        qualifications =  qualifications.concat(e?.qualifications ?? []);
+      }
+      this.selectedData.forEach(outer => {
+        if(!qualifications.find(inner => inner.id == outer.id))
+          qualifications.push(outer);
+      })
+      return qualifications;
+    });
+  }
+
+  private configureTable() {
+    const labels: Label<Qualification>[] = [
       new Label('id', 'ID'),
       new Label('skill', 'Qualification'),
-    ]
-    const selectionBehaviour = new SelectionBehaviour(false,'');
+    ];
+    const selectionBehaviour = new SelectionBehaviour(false, '');
     const routing = new Routing(false, '');
-
     this.configuration = new TableConfiguration<Qualification>(
-      this.qualificationCacheService, labels, true,selectionBehaviour, routing
-    )
+      this.qualificationCache, labels, true, selectionBehaviour, routing
+    );
+  }
+  
+  ngDoCheck(): void {
+    if(this.isComponentDataLoaded)
+      return;
+    this.employeeCache.notifyStateChange();
+    const isCacheStillLoading: boolean = this.employeeCache.isLoading().has(-1);
+    if(!isCacheStillLoading)
+      this.isComponentDataLoaded = true;
   }
 
-  protected readonly signal = signal;
+  updateEmployeeData(data: Employee) {
+    this.formDataEmployee = data;
+  }
+
+  submitDataToBackend() {
+    const employee: Employee = this.formDataEmployee ?? new Employee();
+    employee.id = this.employeeSigal().id;
+    employee.qualifications = this.displayedQualificationsSignal();
+    if(employee.id)
+      this.employeeService.update(employee);
+    else
+      this.employeeService.insert(employee);     
+  }
 }
